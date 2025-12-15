773be639011adacc925b2598131e104946eb1423
Shreyash - Shukla <s.shreyashxp@gmail.com>
2025-12-15 22:52:29 +0530

    setup  the server and express part

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';    
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5001;
+
+app.get('/', (req, res) => {
+  res.send('Expense Tracker Backend is running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
